cf3a5b292ddcf0d5f2ef54fa216130e155ede57b
Vitalii Kyi <kyi.profy@gmail.com>
2023-10-23 13:10:52 +0300

    hw-02.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackButtons } from './FeedbackButtons/FeedbackButtons';
+import { Statistics } from './Statistics/Statistics';
+import { NotificationMessage } from './NotificationMessage/NotificationMessage';
+import { GlobalStyle, Box } from './GlobalStyle';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleChangeStats = badge => {
+    this.setState(prevState => {
+      return {
+        [badge]: prevState[badge] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositivePercentage = () => {
+    const total = this.countTotalFeedback();
+    return total === 0 ? 0 : Math.round((this.state.good / total) * 100);
+  };
+
+  handleResetStats = () => {
+    this.setState({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <Box>
+        <Section title="Please leave your feedback">
+          <FeedbackButtons
+            options={Object.keys(this.state)}
+            onChangeStats={this.handleChangeStats}
+            onClear={this.handleResetStats}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ? (
+            <NotificationMessage message="There is no feedback, sorry!" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              percent={this.countPositivePercentage()}
+            />
+          )}
+        </Section>
+
+        <GlobalStyle />
+      </Box>
+    );
+  }
+}
---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,56 @@
+import { styled } from 'styled-components';
+
+export const Thumb = styled.section`
+  // color: midnightblue;
+
+  h2 {
+    padding: 24px 8px;
+    margin: auto;
+    // margin-bottom: 36px;
+  }
+
+  // b {
+  //   border-bottom: 2px dashed midnightblue;
+  // }
+
+  button {
+    font-weight: bold;
+    margin-bottom: 24px;
+    padding: 8px 12px;
+    border: 1px solid #bcbcbc;
+    border-radius: 4px;
+    text-transform: uppercase;
+    background-color: #d9ead3;
+    // background-color: #93c47d;
+  }
+
+  button:hover,
+  button:focus {
+    background-color: #93c47d;
+    color: white;
+  }
+
+  div {
+    display: flex;
+    justify-content: center;
+    gap: 12px;
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+
+  li p {
+    color: black;
+    font-size: larger;
+    font-style: oblique;
+  }
+
+  li span {
+    font-weight: bold;
+    font-size: large;
+    font-style: normal;
+  }
+`;
